425915c49c991f1e921c0bb69b01d27d8400e215
Lance Chao <41135423+lance27866854@users.noreply.github.com>
2019-01-13 13:33:16 +0800

    Update testcase_generator.cpp

--- testcase_generator.cpp
+++ testcase_generator.cpp
@@ -11,6 +11,20 @@
 #define MIN_COST 0
 #define MAX_DIFF 100
 #define MIN_DIFF 0
+// weight define
+//nodes
+#define A_WEIGHT 500
+#define B_WEIGHT 400
+#define C_WEIGHT 300
+#define D_WEIGHT 200
+#define E_WEIGHT 100
+#define F_WEIGHT 0
+//links
+#define X_WEIGHT 500
+#define Y_WEIGHT 400
+#define Z_WEIGHT 300
+#define W_WEIGHT 200
+#define S_WEIGHT 10
 
 using namespace std;
 ofstream out_file;
@@ -79,6 +93,101 @@ class TCG{
                 }
             }
         }
+        void generate_tricky_graph(int nodes, int budget, int start_time){
+            //nodes = 4n+1
+            int links = (nodes-1)*6/4;
+            int right_blocks = (((nodes-1)/4)+1)/2;
+            int left_blocks = (((nodes-1)/4))/2;
+            int blocks = ((nodes-1)/4);
+
+            out_file<<nodes<<" "<<links<<" "<<budget<<" "<<start_time<<"\n";
+            ///nodes
+            int happiness_init = A_WEIGHT;
+            int op_time_init = start_time+(X_WEIGHT + Y_WEIGHT + 2*S_WEIGHT)*(right_blocks)-5;
+            int close_time_init = start_time+(X_WEIGHT + Y_WEIGHT + 2*S_WEIGHT)*(right_blocks)+5;
+            out_file<<name_encoder(0)<<" "<<happiness_init<<" "<<op_time_init<<" "<<close_time_init<<"\n";
+
+            int back_time = start_time + (X_WEIGHT + S_WEIGHT)*right_blocks;
+            //right
+            for(int i=0;i<right_blocks;i++){//route
+                int start_pt = i*4;
+
+                int happiness1 = B_WEIGHT;
+                int op_time_1 = start_time+(X_WEIGHT*(i+1)+S_WEIGHT*i)-5;
+                int close_time_1 = start_time+(X_WEIGHT*(i+1)+S_WEIGHT*i)+5;
+                out_file<<name_encoder(start_pt+1)<<" "<<happiness1<<" "<<op_time_1<<" "<<close_time_1<<"\n";
+
+                int happiness2 = C_WEIGHT;
+                int op_time_2 = back_time+(S_WEIGHT*(right_blocks-i)+Y_WEIGHT*(right_blocks-i-1))-5;
+                int close_time_2 = back_time+(S_WEIGHT*(right_blocks-i)+Y_WEIGHT*(right_blocks-i-1))+5;
+                out_file<<name_encoder(start_pt+2)<<" "<<happiness2<<" "<<op_time_2<<" "<<close_time_2<<"\n";
+
+                int happiness3 = D_WEIGHT;
+                int op_time_3 = start_time+(X_WEIGHT*(i+1)+S_WEIGHT*i)-5;
+                int close_time_3 = start_time+(X_WEIGHT*(i+1)+S_WEIGHT*i)+5;
+                out_file<<name_encoder(start_pt+3)<<" "<<happiness3<<" "<<op_time_3<<" "<<close_time_3<<"\n";
+
+                int happiness4 = A_WEIGHT;
+                int op_time_4 = (start_time+(X_WEIGHT+S_WEIGHT)*(i+1))-5;
+                int close_time_4 = (start_time+(X_WEIGHT+S_WEIGHT)*(i+1))+5;
+                out_file<<name_encoder(start_pt+4)<<" "<<happiness4<<" "<<op_time_4<<" "<<close_time_4<<"\n";
+            }
+            //left
+            for(int i=0;i<left_blocks;i++){//barrier
+                int start_pt = (i+right_blocks)*4;
+                if(i<=left_blocks/2){
+                    int happiness1 = F_WEIGHT;
+                    out_file<<name_encoder(start_pt+1)<<" "<<happiness1<<" 0 10000\n";
+                    int happiness2 = F_WEIGHT;
+                    out_file<<name_encoder(start_pt+2)<<" "<<happiness2<<" 0 10000\n";
+                    int happiness3 = A_WEIGHT;
+                    out_file<<name_encoder(start_pt+3)<<" "<<happiness3<<" 0 10000\n";
+                    int happiness4 = A_WEIGHT;
+                    out_file<<name_encoder(start_pt+4)<<" "<<happiness4<<" 0 10000\n";
+                }
+                else{
+                    int happiness1 = F_WEIGHT;
+                    out_file<<name_encoder(start_pt+1)<<" "<<happiness1<<" 0 10\n";
+                    int happiness2 = F_WEIGHT;
+                    out_file<<name_encoder(start_pt+2)<<" "<<happiness2<<" 0 10\n";
+                    int happiness3 = E_WEIGHT;
+                    out_file<<name_encoder(start_pt+3)<<" "<<happiness3<<" 0 10\n";
+                    int happiness4 = E_WEIGHT;
+                    out_file<<name_encoder(start_pt+4)<<" "<<happiness4<<" 0 10\n";
+                }
+            }
+            ///links
+            //right
+            for(int i=0;i<right_blocks;i++){
+                int start_pt = i*4;
+                int end_pt = (i+1)*4;
+                out_file<<name_encoder(start_pt)<<" "<<name_encoder(start_pt+1)<<" "<<X_WEIGHT<<"\n";
+                out_file<<name_encoder(start_pt+1)<<" "<<name_encoder(end_pt)<<" "<<S_WEIGHT<<"\n";
+
+                out_file<<name_encoder(start_pt)<<" "<<name_encoder(start_pt+2)<<" "<<Y_WEIGHT<<"\n";
+                out_file<<name_encoder(start_pt+2)<<" "<<name_encoder(end_pt)<<" "<<S_WEIGHT<<"\n";
+
+                out_file<<name_encoder(start_pt)<<" "<<name_encoder(start_pt+3)<<" "<<X_WEIGHT<<"\n";
+                out_file<<name_encoder(start_pt+3)<<" "<<name_encoder(end_pt)<<" "<<X_WEIGHT<<"\n";
+            }
+            //left
+            int start_pt = 0;
+            int end_pt = (right_blocks+1)*4;
+            for(int j=3;j>=1;j--){
+                int cost = W_WEIGHT;
+                out_file<<name_encoder(start_pt)<<" "<<name_encoder(end_pt-j)<<" "<<cost<<"\n";
+                out_file<<name_encoder(end_pt-j)<<" "<<name_encoder(end_pt)<<" "<<cost<<"\n";
+            }
+            for(int i=1;i<left_blocks;i++){
+                start_pt = (i+right_blocks)*4;
+                end_pt = (i+right_blocks+1)*4;
+                for(int j=1;j<=3;j++){
+                    int cost = X_WEIGHT;
+                    out_file<<name_encoder(start_pt)<<" "<<name_encoder(start_pt+j)<<" "<<cost<<"\n";
+                    out_file<<name_encoder(start_pt+j)<<" "<<name_encoder(end_pt)<<" "<<cost<<"\n";
+                }
+            }
+        }
         void check(){
             int nodes, links;
             int budget, start_time;
@@ -144,8 +253,9 @@ int main(void){
     //write.
     out_file.open(OUT_FILE_NAME, ios::out);
     TCG tcg;
-    //tcg.generate_skewed_graph(97, 1000, 480);
-    tcg.generate_multiroute_graph(97, 10000, 480);
+    //tcg.generate_skewed_graph(100, 1000, 480);
+    //tcg.generate_multiroute_graph(97, 10000, 480);
+    tcg.generate_tricky_graph(97, 11520, 480);
     out_file.close();
     //read.
     in_file.open(IN_FILE_NAME, ios::in);
